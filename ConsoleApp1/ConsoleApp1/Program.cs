800cb8ca0ef49ab524114083f5ece712bf04e5d1
doradca3012 <zakupyonlinemk@gmail.com>
2020-02-17 21:15:58 +0100

    test zmiany

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        int tree;
+        Console.WriteLine("Jak wysoka ma być choinka:");
+        do
+        {
+            string input = Console.ReadLine();
+             tree = int.Parse(input);
+            for (tree = 1; tree < 10; tree++)
+            {
+                Console.WriteLine(tree);
+            }
+            //test
+        } while (tree != 0);
+    }
+}
