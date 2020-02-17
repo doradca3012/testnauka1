5dbfd21f517afb8dace73f68d3ac931096e8d8f3
doradca3012 <zakupyonlinemk@gmail.com>
2020-02-17 21:02:06 +0100

    choinka

--- choinka/choinka/Program.cs
+++ choinka/choinka/Program.cs
@@ -1,4 +1,4 @@
-﻿using System; 
+﻿using System;
 
 public class Example
 {
@@ -7,7 +7,7 @@ public class Example
         string userInput;
         Console.WriteLine("Jak wysoka ma być choinka?");
         userInput = Console.ReadLine();
-        bool isNumber = int.TryParse(userInput, out var high); 
+        bool isNumber = int.TryParse(userInput, out var high);
         if (isNumber == false)
         {
             Console.WriteLine("Nie sciemaniaj, podaj liczbę");
@@ -16,8 +16,11 @@ public class Example
         {
             for (int i = 0; i < high; i++)
             {
-                Console.WriteLine("*");
+                string ile = new string('*', i+1);
+                string spacja = new string(' ', high);
+                Console.WriteLine(spacja+ile);
             }
         }
+        Console.ReadKey();
     }
 }
